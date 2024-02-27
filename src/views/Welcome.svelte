6633f2ec84ea62e85c9fbd0b62398ce8b24425d7
andres-vizcaino <andres.vizcaino@igloolab.co>
2024-02-27 17:04:45 -0500

    Update game step and add footer in Welcome view

--- src/views/Welcome.svelte
+++ src/views/Welcome.svelte
@@ -19,4 +19,21 @@
   />
 
   <Button on:click={() => step.set(GameStatus.Instructions)}>Iniciar</Button>
+
+  <footer class="text-center text-xs text-balance">
+    Nucala Pluma (Mepolizumab) Solución inyectable 100mg/mL | Registro sanitario
+    No: INVIMA 2022MBT-0000061 | Material exclusivo para profesionales de la
+    salud | Si usted desea mayor información o reportar una situación clínica
+    desfavorable ocurrida durante el uso de un producto de GlaxoSmithKline,
+    favor comunicarse al teléfono 01 8000 11 86 86 o escribir al correo
+    electrónico: programa.dirmedica@gsk.com | GlaxoSmithKline Colombia S.A.,
+    Calle 26 # 69B-45 Edificio Bogotá Corporate Center Piso 9. | Solicite mayor
+    información científica de nuestros productos en nuestro servicio de
+    información médica, a través del email mila@gsk.com | Mayor información
+    sobre indicaciones, contraindicaciones, advertencias y precauciones, riesgos
+    de administración y demás información sobre seguridad, se encuentra
+    disponible en la Información para Prescribir (IP). IP y/o referencias están
+    disponibles a solicitud. | PM-CO-MPL-BTHC-240003 | Fecha de elaboración:
+    Febrero 2024.
+  </footer>
 </section>
